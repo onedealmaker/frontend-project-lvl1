f74898a8d2da6dac8bf031d1345c26dec45436dd
onedealmaker <dealmaker@HOME-PC.localdomain>
2022-01-17 22:58:00 +0300

    fix linter check

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -7,9 +7,9 @@ const getProgression = (progressionLength, firstMember, step) => {
   const progression = [];
   for (let memberIndex = 0; memberIndex < progressionLength; memberIndex += 1) {
     const lastMember = progression[progression.length - 1];
-    if(progression.length < 1) {
+    if (progression.length < 1) {
       progression.push(firstMember);
-    } else {progression.push(lastMember + step)}
+    } else { progression.push(lastMember + step); }
   }
   return progression;
 };
@@ -22,7 +22,7 @@ const getQuestion = (progression, answerIndex) => {
 
 const generateQuestionAndAnswer = () => {
   const progressionLength = 10;
-  const step = getRandomNumber(5, 21)
+  const step = getRandomNumber(5, 21);
   const firstMember = getRandomNumber(1, 10);
   const progression = getProgression(progressionLength, firstMember, step);
   const answerIndex = getRandomNumber(0, progressionLength - 1);
