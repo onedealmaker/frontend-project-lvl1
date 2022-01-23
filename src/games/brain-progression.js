c0f3e76c9e124bde77378b5a324425d616952f0f
onedealmaker <dealmaker@HOME-PC.localdomain>
2022-01-23 14:20:28 +0300

    remove condition from getProgression, randomize progression length

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,12 +4,11 @@ import getRandomNumber from '../randomNumber.js';
 const description = 'What number is missing in the progression?';
 
 const getProgression = (progressionLength, firstMember, step) => {
-  const progression = [];
-  for (let memberIndex = 0; memberIndex < progressionLength; memberIndex += 1) {
-    const lastMember = progression[progression.length - 1];
-    if (progression.length < 1) {
-      progression.push(firstMember);
-    } else { progression.push(lastMember + step); }
+  const progression = [firstMember];
+  for (let newMemberIndex = progression.length; newMemberIndex < progressionLength; newMemberIndex += 1) {
+    const lastMember = progression[newMemberIndex - 1];
+    const newMember = lastMember + step;
+    progression.push(newMember);
   }
   return progression;
 };
@@ -21,7 +20,7 @@ const getQuestion = (progression, answerIndex) => {
 };
 
 const generateQuestionAndAnswer = () => {
-  const progressionLength = 10;
+  const progressionLength = getRandomNumber(5, 10);
   const step = getRandomNumber(5, 21);
   const firstMember = getRandomNumber(1, 10);
   const progression = getProgression(progressionLength, firstMember, step);
